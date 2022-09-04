ea110281756a1f1667c4d706097128064bd6bb3d
Chaitanya5599 <100076633+Chaitanya5599@users.noreply.github.com>
2022-09-04 09:06:22 +0530

    Update area.c

--- area.c
+++ area.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main() {
- int a;
- a = 2;
+ int r;
+ r = 2;
 
- printf("area of circle is :- %f", 3.14*a*a); 
-
+ printf("area of circle is :- %f", 3.14*r*r); // by using different formula we can perform other areas 
+ // printf("area of triangle is :- %f", 1/2*l*b);     area of triangle
+ // printf("area of rectangle  is :- %f ,l*b);          area od rectangle
 return 0;
 }
