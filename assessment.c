189b8e624194c5e1c0e820cd42bab1c453468c4f
Chaitanya Patel <100076633+Chaitanya5599@users.noreply.github.com>
2022-09-29 16:18:48 +0530

    Add files via upload

---
+++ assessment.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main(){
+    int Ch;
+    char S[100];
+    
+    printf("Main Menu\n");
+    printf("    1. Reverse\n" );
+    printf("    2. concatation\n" );
+    printf("    3. Palindrome\n" );
+    printf("    4. Copy string\n" );
+    printf("    5. length of String\n" );
+    printf("    6. Frequency of string\n" );
+    printf("    7. No. of vowels and consonants\n" );
+    printf("    8. No. of blank spaces and digits\n" );
+    printf("    9. exit\n" );
+
+    printf("Enter string:-  ");
+    gets("s");
+
+    printf("Enter your Choice :- ");
+    scanf("%d",&Ch);
+    
+
+
+
+  
+    switch(Ch)
+    {
+
+        
+    default:
+    printf("enter correct value");
+        
+    }
+
+  
+    return 0;
+}
