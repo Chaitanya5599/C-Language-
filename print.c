d8dbe5ed03f64998fa0f2daccb7a2fb6bad96bb9
Chaitanya Patel <100076633+Chaitanya5599@users.noreply.github.com>
2022-10-16 21:56:24 +0530

    lol

--- print.c
+++ print.c
@@ -3,7 +3,7 @@ int main() {
  printf("patel chaitanya \n");
  printf(" 17 \n");
  printf("25-10-2004 \n");
- printf(" water park soc, pipodara, MAngrol, Surat \n");
+ printf(" water park soc, pipodara, Mangrol, Surat \n");
 
 return 0;
 }
